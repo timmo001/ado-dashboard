8cb73ce534222f072aa0b8734d3c334b49b19640
Aidan Timson <contact@timmo.xyz>
2022-02-21 09:49:30 +0000

    v4 to v5 theme

--- components/Theme.tsx
+++ components/Theme.tsx
@@ -1,51 +1,24 @@
-import {
-  createTheme,
-  responsiveFontSizes,
-  adaptV4Theme,
-} from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import { cyan, indigo, purple } from "@mui/material/colors";
 
-let theme = createTheme(
-  adaptV4Theme({
-    palette: {
-      mode: "dark",
-      primary: cyan,
-      secondary: indigo,
-      contrastThreshold: 3,
-      tonalOffset: 0.2,
-    },
-    overrides: {
-      MuiCard: {
-        root: {
-          margin: 8,
-        },
-      },
-      MuiCardContent: {
-        root: {
-          padding: "24px 32px",
-          "&:last-child": {
-            paddingBottom: 16,
-          },
-        },
-      },
-      MuiChip: {
-        root: {
-          margin: 4,
-        },
-      },
-      MuiCardActions: {
-        root: {
-          justifyContent: "flex-end",
-        },
-      },
-      MuiLink: {
+let theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: cyan,
+    secondary: indigo,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+  components: {
+    MuiLink: {
+      styleOverrides: {
         root: {
           color: purple[400],
         },
       },
     },
-  })
-);
+  },
+});
 theme = responsiveFontSizes(theme);
 
 export default theme;
